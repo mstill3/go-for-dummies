1942cbec7ecef1eb7835b29dba0c79900cc2a4f4
Matt Stillwell <Matthew.Stillwell@jacobs.com>
2023-01-26 12:35:40 -0500

    :sparkles: Add example

--- src/16_stringer.go
+++ src/16_stringer.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// NOTE: uses Person struct in `12_structs.go`
+// similar to Java toString()
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.name, p.age)
+}
+
 func myStringer() {
-	fmt.Println("Stringer")
+	// Stringer is a very common interface
+	// A Stringer is a type that can describe itself as a string
+	person1 := Person{name: "Matt", age:25}
+	fmt.Println(person1)
 }
